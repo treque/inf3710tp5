f52be626e7eee5b562f55dc3dd3d7eaaa8fa3f80
Huyen Trang Dinh <tran9dinh@gmail.com>
2019-04-10 19:54:07 -0400

    LLALALLALALA

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Location } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { Hotel } from "../../../common/tables/Hotel";
 import { CommunicationService } from "./communication.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.css",],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     public route: string;
 
     public constructor(private communicationService: CommunicationService, location: Location, router: Router) {
@@ -23,19 +22,6 @@ export class AppComponent implements OnInit {
     }
 
     public readonly title: string = "INF3710 TP5";
-    public hotels: Hotel[] = [];
-    public ngOnInit(): void {
-        this.communicationService.listen().subscribe((m:any) => {
-            console.log(m);
-            this.getHotels();
-        });
-    }
-
-    public getHotels(): void {
-        this.communicationService.getHotels().subscribe((hotels: Hotel[]) => {
-            this.hotels = hotels;
-        });
-    }
 
     public createDB(): void {
         this.communicationService.setUpDatabase().subscribe((res: any) => {
