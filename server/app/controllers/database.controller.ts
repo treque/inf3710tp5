de3b17a0bb67dc76a9a91d701481e9335648738c
Helene <lgrthmntrl@gmail.com>
2019-04-13 20:30:33 -0400

    suppression server

--- server/app/controllers/database.controller.ts
+++ server/app/controllers/database.controller.ts
@@ -15,7 +15,7 @@ export class DatabaseController {
         router.post("/createSchema",
                     (req: Request, res: Response, next: NextFunction) => {
                     this.databaseService.createSchema().then((result: pg.QueryResult) => {
-                        console.log("CECI EST UNE FONCTION DE TEST SEULEMENT");
+                        console.log("Schéma créé");
                         res.json(result);
                     }).catch((e: Error) => {
                         console.error(e.stack);
@@ -25,39 +25,13 @@ export class DatabaseController {
         router.post("/populateDb",
                     (req: Request, res: Response, next: NextFunction) => {
                     this.databaseService.populateDb().then((result: pg.QueryResult) => {
-                        console.log("CECI EST UNE FONCTION DE TEST SEULEMENT");
+                        console.log("DB populée");
                         res.json(result);
                     }).catch((e: Error) => {
                         console.error(e.stack);
                     });
         });
-/*
-        router.get("/hotel",
-                   (req: Request, res: Response, next: NextFunction) => {
-                    // Send the request to the service and send the response
-                    this.databaseService.getHotels().then((result: pg.QueryResult) => {
-                    const hotels: Hotel[] = result.rows.map((hot: any) => (
-                        {
-                        hotelno: hot.hotelno,
-                        hotelname: hot.hotelname,
-                        city: hot.city
-                    }));
-                    res.json(hotels);
-                }).catch((e: Error) => {
-                    console.error(e.stack);
-                });
-            });
 
-        router.get("/hotel/hotelNo",
-                   (req: Request, res: Response, next: NextFunction) => {
-                      this.databaseService.getHotelNo().then((result: pg.QueryResult) => {
-                        const hotelPKs: string[] = result.rows.map((row: any) => row.hotelno);
-                        res.json(hotelPKs);
-                      }).catch((e: Error) => {
-                        console.error(e.stack);
-                    });
-                  });
-*/
         router.post("/animal/insert",
                     (req: Request, res: Response, next: NextFunction) => {
                         const animId: string = req.body.animid;
@@ -147,56 +121,7 @@ export class DatabaseController {
            console.error(e.stack);
        });
      });
-/*
-        router.get("/rooms",
-                   (req: Request, res: Response, next: NextFunction) => {
 
-                    // this.databaseService.getRoomFromHotel(req.query.hotelNo, req.query.roomType, req.query.price)
-                    this.databaseService.getRoomFromHotelParams(req.query)
-                    .then((result: pg.QueryResult) => {
-                        const rooms: Room[] = result.rows.map((room: Room) => (
-                            {
-                            hotelno: room.hotelno,
-                            roomno: room.roomno,
-                            typeroom: room.typeroom,
-                            price: parseFloat(room.price.toString())
-                        }));
-                        res.json(rooms);
-                    }).catch((e: Error) => {
-                        console.error(e.stack);
-                    });
-            });
-
-        router.post("/rooms/insert",
-                    (req: Request, res: Response, next: NextFunction) => {
-                    const room: Room = {
-                        hotelno: req.body.hotelno,
-                        roomno: req.body.roomno,
-                        typeroom: req.body.typeroom,
-                        price: parseFloat(req.body.price)};
-                    console.log(room);
-
-                    this.databaseService.createRoom(room)
-                    .then((result: pg.QueryResult) => {
-                        res.json(result.rowCount);
-                    })
-                    .catch((e: Error) => {
-                        console.error(e.stack);
-                        res.json(-1);
-                    });
-        });
-
-        router.get("/tables/:tableName",
-                   (req: Request, res: Response, next: NextFunction) => {
-                this.databaseService.getAllFromTable(req.params.tableName)
-                    .then((result: pg.QueryResult) => {
-                        res.json(result.rows);
-                    }).catch((e: Error) => {
-                        console.error(e.stack);
-                    });
-        });
-        */
-        // partie vsf
         router.post("/treatment", (req: Request, res: Response, next: NextFunction) => {
             this.databaseService.getTreatmentsById(req.body.animId, req.body.ownerId, req.body.clinicId)
             .then((result: pg.QueryResult) => {
